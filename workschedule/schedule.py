cd30be5be3f68bd1cc8a77dec6e57b611bf10e14
Jurek <jurek.elliesen@web.de>
2021-10-15 14:34:20 +0200

    Add history

--- workschedule/schedule.py
+++ workschedule/schedule.py
@@ -1,86 +1,75 @@
+import ntpath
+import pickle
 from pathlib import Path
 
-import helpers
-import timer
+import work_timer
+import working_history as history
 
+# TODO: output
 # topic -> hours to work
-schedule = {}
-# topic -> hours worked
-worked = {}
-# topic -> hours to work, remaining since last reset
-carry = {}
-
-work_timer = timer.WorkTimer()
+schedule: dict = {}
+# topic -> hours remaining
+remaining: dict = {}
+work_timer = work_timer.WorkTimer()
 
 
-def build_schedule(fpath) -> None:
-    """Creates schedule from txt file.
+def from_file(fpath: str) -> None:
+    """Initializes schedule and history files.
 
-    Each topic is on single line and topic name and hours are seperated by
-    colon followed by a whitespace.
+    Creates a .schedule and a .history file, with same name as given file.
 
     Parameters
     ----------
-    fpath : str
-        Relative path to file.
+    fpath
+        Path to file.
     """
-    fpath = helpers.get_top_directory() / fpath
+    fpath = Path(fpath)
     if not fpath.is_file():
-        raise ValueError(f"could not find schedule at {Path(fpath).absolute()}")
+        raise FileNotFoundError(f"could not find file {fpath.absolute()}")
 
+    schedule = {}
+    remaining = {}
     with open(fpath, "r") as file:
-        for line in file.readlines():
-            name, hours = [x.rstrip("\n ") for x in line.split(": ")]
-            schedule[name] = float(hours)
-            worked[name] = 0
-            carry[name] = 0
+        lines = file.readlines()
+        for line in lines:
+            topic, hours = line.split(": ")
+            hours = hours.rstrip("\n")
+            schedule[topic] = float(hours)
+            remaining[topic] = 0
+
+    name = ntpath.basename(fpath)
+    if "." in name:
+        name = name.split(".")[0]
+    with open(fpath.parent / f"{name}.schedule", "w+b") as file:
+        pickle.dump([schedule, remaining], file)
+    with open(fpath.parent / f"{name}.history", "w+b") as file:
+        pickle.dump([[]], file)
 
 
-def reset(hard=True, negative_carry=False) -> None:
-    """Sets worked hours to 0.
-
+def reset(carry_on: list[str] = None) -> None:
+    """
     Parameters
     ----------
-    hard : bool or list of str, optional
-        If False carry on hours.
-        If list of strings carry on hours for given topics.
-    negative_carry : bool, optional
-        Allow negative values for carry on.
+    carry_on
+        Add remaining hours to work for each topic given as string. Elements
+        which do not correspond to a topic have no effect.
     """
+    if carry_on is None:
+        carry_on = []
+
     for topic in schedule:
-        if isinstance(hard, list):
-            reset_hard = topic not in hard
+        if topic in carry_on:
+            remaining[topic] += schedule[topic] - history.get_hours(topic)
         else:
-            reset_hard = hard
-        if not reset_hard:
-            carry[topic] = schedule[topic] - worked[topic]
-            if carry[topic] < 0 and not negative_carry:
-                carry[topic] = 0
-        else:
-            carry[topic] = 0
-        worked[topic] = 0
+            remaining[topic] = 0
+    history.history.append([])
 
 
 def work(topic, hours) -> None:
-    """
-    Parameters
-    ----------
-    topic : str
-        Topic worked on.
-    hours : float
-        Number of hours. 1.5 hours correspond to 90 minutes.
-    """
-    worked[topic] += hours
+    history.add_entry(history.Entry(topic, hours))
 
 
 def start_working(topic):
-    """
-
-    Parameters
-    ----------
-    topic : str
-        Start timer for current topic.
-    """
     work_timer.start(topic)
 
 
@@ -88,13 +77,3 @@ def stop_working():
     """Stops working timer and adds worked hours."""
     work_timer.stop()
     work(work_timer.topic, round(work_timer.hours(), 1))
-
-
-# TODO: output, history
-if __name__ == '__main__':
-    build_schedule("test.txt")
-    work("Kacken", 10)
-    work("CogScie", 20)
-    work("Pipi", 5)
-    reset(False, True)
-    pass
