187736f4bc877e2669f8d27af52aeaac9e6816c4
Jurek <jurek.elliesen@web.de>
2021-10-15 14:32:19 +0200

    WorkTimer is now a static class

--- workschedule/timer.py
+++ workschedule/timer.py
@@ -2,15 +2,38 @@ from datetime import datetime
 
 
 class WorkTimer:
+    """Measures working time on a topic.
 
-    def start(self, topic):
-        self.topic = topic
-        self.tic = datetime.now()
+    Can only stop time for one topic. Before starting a time on new topic stop
+    has to be called.
+    """
 
-    def stop(self):
-        self.toc = datetime.now()
+    tic: datetime
+    toc: datetime
+    topic: str = None
 
-    def hours(self):
-        diff = self.toc - self.tic
+    @staticmethod
+    def start(topic):
+        if WorkTimer.topic is not None:
+            raise ValueError(f"there is already a timer running on topic"
+                             f"{WorkTimer.topic}")
+        WorkTimer.topic = topic
+        WorkTimer.tic = datetime.now()
+
+    @staticmethod
+    def stop():
+        if WorkTimer.topic is None:
+            raise ValueError("there is no active timer")
+        WorkTimer.toc = datetime.now()
+        WorkTimer.topic = None
+
+    @staticmethod
+    def pause():
+        # TODO
+        pass
+
+    @staticmethod
+    def hours():
+        diff = WorkTimer.toc - WorkTimer.tic
         seconds_in_hour = 60 * 60
         return diff.seconds / seconds_in_hour
