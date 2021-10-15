f3b6040715236e12c5e161cc76246b9445b1d5e8
Jurek <jurek.elliesen@web.de>
2021-10-15 13:55:20 +0200

    Add method to get hours worked on topic

--- tests/test_history.py
+++ tests/test_history.py
@@ -5,9 +5,32 @@ from workschedule import working_history as history
 
 class TestHistory(unittest.TestCase):
 
-    def test_add_entry(self):
+    def test_add_entry(self) -> None:
         # same topic, same day
         # same topic, not same day
         # not same topic, same day
-        a = history.Entry("a", 1)
-        b = history.Entry("")
+        a = history.Entry("b", 1)
+        b = history.Entry("b", 2)
+        c = history.Entry("a", 3)
+        c.date = "01/02/0001"
+        d = history.Entry("a", 4)
+
+        history.add_entry(a)
+        history.add_entry(b)
+        self.assertEqual(len(history.history[-1]), 1)
+        history.add_entry(c)
+        self.assertEqual(len(history.history[-1]), 2)
+        history.add_entry(d)
+        self.assertEqual(history.history[-1][0], c)
+        self.assertEqual(history.history[-1][1], d)
+        self.assertEqual(history.history[-1][2].hours, 3)
+
+    def test_get_hours(self) -> None:
+        history.add_entry(history.Entry("b", 1))
+        history.add_entry(history.Entry("c", 2))
+        history.add_entry(history.Entry("a", 3))
+        history.add_entry(history.Entry("a", 4))
+
+        self.assertEqual(history.get_hours("a"), 7)
+        self.assertEqual(history.get_hours("b"), 1)
+        self.assertEqual(history.get_hours("c"), 2)
--- workschedule/working_history.py
+++ workschedule/working_history.py
@@ -1,24 +1,40 @@
 from dataclasses import dataclass, field
+from datetime import datetime
 
-history = []
+history = [[]]
 
 
 def add_entry(entry):
-    if entry in history:
-        history[history.index(entry)].hours += entry.hours
+    """Adds entry to history.
+
+    Two entries with same date and topic will be merged, so that each topic can
+    only occur once in current period.
+    """
+    curr_period = history[-1]
+    if entry in curr_period:
+        curr_period[curr_period.index(entry)].hours += entry.hours
     else:
-        history.append(entry)
-    history.sort()
+        curr_period.append(entry)
+    curr_period.sort()
+
+
+def get_hours(topic: str) -> float:
+    """Returns number of hours worked in current period on topic."""
+    curr_period = history[-1]
+    hours = 0
+    for entry in curr_period:
+        if entry.topic == topic:
+            hours += entry.hours
+    return hours
 
 
 @dataclass(order=True)
 class Entry:
-    prim_sort_index: str = field(init=False, repr=False)
     sec_sort_index: str = field(init=False, repr=False)
     topic: str
     hours: float = field(compare=False)
-    date: str
+    date: str = field(init=False)
 
     def __post_init__(self):
-        self.prim_sort_index = self.date
+        self.date = datetime.now().strftime("%d/%m/%Y")
         self.sec_sort_index = self.topic
