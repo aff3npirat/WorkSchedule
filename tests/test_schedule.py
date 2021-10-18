b0eebc0ab282e0444c32f76dd619e4fac7fb2165
Jurek <jurek.elliesen@web.de>
2021-10-18 18:08:37 +0200

    Implement test_reset

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -1,6 +1,31 @@
 import unittest
 
+import history
+import schedule
+import work_timer
+
 
 class TestSchedule(unittest.TestCase):
-    # TODO
-    pass
+    in_file = "./test_schedule.txt"
+
+    def setUp(self) -> None:
+        schedule.schedule = {}
+        schedule.remaining = {}
+        schedule.work_timer = work_timer.WorkTimer()
+        schedule.from_file(self.in_file)
+        schedule.load("test_schedule")
+
+    def test_reset(self):
+        schedule.work("Lernen", 6.5)
+        schedule.work("Arbeiten", 14)
+        schedule.work("LustigeSachen", 17)
+        schedule.reset(["Arbeiten", "Lernen"])
+
+        actual = schedule.remaining
+        expected = {"Lernen": -1.5, "Arbeiten": 6.0, "LustigeSachen": 0.0}
+        self.assertEqual(actual, expected)
+        self.assertEqual(len(history.history), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
