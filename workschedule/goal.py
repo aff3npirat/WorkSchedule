ebef0ec9310086df7e7cc09408fd39920fb248a1
Jurek <jurek.elliesen@web.de>
2021-10-19 14:10:17 +0200

    Implement __eq__

--- workschedule/goal.py
+++ workschedule/goal.py
@@ -1,9 +1,13 @@
 from dataclasses import dataclass
 
 
-@dataclass()
+@dataclass(eq=False)
 class Goal:
+    """A goal can be a task or note."""
     name: str
     description: str
-    topic: str
     periodic: bool
+    done: bool = False
+
+    def __eq__(self, other):
+        return self.name == other
