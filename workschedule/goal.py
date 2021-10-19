029064799f0511d02e653b877c79e4886c92de21
Jurek <jurek.elliesen@web.de>
2021-10-19 14:54:42 +0200

    Implement get_not_dones, _get_goals

--- workschedule/goal.py
+++ workschedule/goal.py
@@ -6,8 +6,20 @@ class Goal:
     """A goal can be a task or note."""
     name: str
     description: str
-    periodic: bool
     done: bool = False
 
     def __eq__(self, other):
         return self.name == other
+
+
+def get_not_dones(goals: list[Goal]) -> list[Goal]:
+    """Retursn all done goals."""
+    return _get_goals(goals, "done", False)
+
+
+def _get_goals(goals: list[Goal], attrib: str, value) -> list[Goal]:
+    to_return = []
+    for goal_ in goals:
+        if Goal.__getattribute__(goal_, attrib) == value:
+            to_return.append(goal_)
+    return to_return
