fa6d74f8b3709018c58739a3e9713b1f2c6d3050
Jurek <jurek.elliesen@web.de>
2021-10-20 17:06:15 +0200

    Remove defaults from positional arguments

--- workschedule/terminal_interface.py
+++ workschedule/terminal_interface.py
@@ -85,18 +85,12 @@ parser_overview.add_argument("-d",
 parser_overview.set_defaults(func=overview)
 
 parser_add_topic = subparsers.add_parser("add", help="add_topic help")
-parser_add_topic.add_argument("topic", default="", type=str, help="topic help")
-parser_add_topic.add_argument("hours",
-                              default=None,
-                              type=float,
-                              help="hours help")
+parser_add_topic.add_argument("topic", type=str, help="topic help")
+parser_add_topic.add_argument("hours", type=float, help="hours help")
 parser_add_topic.set_defaults(func=add_topic)
 
 parser_remove_topic = subparsers.add_parser("remove", help="remove_topic help")
-parser_remove_topic.add_argument("topic",
-                                 default="",
-                                 type=str,
-                                 help="topic help")
+parser_remove_topic.add_argument("topic", type=str, help="topic help")
 parser_remove_topic.set_defaults(func=remove_topic)
 
 parser_work = subparsers.add_parser("work", help="work help")
@@ -109,7 +103,6 @@ parser_workt.add_argument("-t", "--topic", default="", type=str, help="-t help")
 parser_workt.add_argument("-s",
                           "--stop",
                           default=False,
-                          type=bool,
                           action="store_true",
                           help="-s help")
 
