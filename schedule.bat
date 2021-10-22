e118d470b8e02e3592cf08822caa01f242045bde
Jurek <jurek.elliesen@web.de>
2021-10-22 20:50:29 +0200

    Add bat file

---
+++ schedule.bat
@@ -0,0 +1,5 @@
+@echo off
+set pythonexe=c:\miniconda3\envs\workschedule\python.exe
+set script=c:\Users\jurek\Desktop\projects\python\WorkSchedule\workschedule\terminal_interface.py
+
+%pythonexe% %script% %*
