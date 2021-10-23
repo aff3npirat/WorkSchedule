b28f824654f80d88ab7fbd3390ad3dc5d5790036
Jurek <jurek.elliesen@web.de>
2021-10-23 12:59:23 +0200

    Made WorkTimer non static

--- workschedule/work_timer.py
+++ workschedule/work_timer.py
@@ -10,32 +10,26 @@ class NoTimerActive(Exception):
 
 
 class WorkTimer:
-    """Measures working time on a topic.
+    """Measures working time on a topic."""
 
-    Can only stop time for one topic. Before starting a time on new topic stop
-    has to be called.
-    """
+    def __init__(self):
+        self.tic: datetime = None
+        self.toc: datetime = None
+        self.topic: str = None
 
-    tic: datetime
-    toc: datetime
-    topic: str = None
+    def start(self, topic):
+        if self.topic is not None:
+            raise TimerAlreadyRunning(self.topic)
+        self.topic = topic
+        self.tic = datetime.now()
 
-    @staticmethod
-    def start(topic):
-        if WorkTimer.topic is not None:
-            raise TimerAlreadyRunning(WorkTimer.topic)
-        WorkTimer.topic = topic
-        WorkTimer.tic = datetime.now()
-
-    @staticmethod
-    def stop():
-        if WorkTimer.topic is None:
+    def stop(self):
+        if self.topic is None:
             raise NoTimerActive
-        WorkTimer.toc = datetime.now()
-        WorkTimer.topic = None
+        self.toc = datetime.now()
+        self.topic = None
 
-    @staticmethod
-    def hours():
-        diff = WorkTimer.toc - WorkTimer.tic
+    def hours(self):
+        diff = self.toc - self.tic
         seconds_in_hour = 60 * 60
         return diff.seconds / seconds_in_hour
